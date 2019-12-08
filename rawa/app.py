7a09b8275978ab4caedb327f7aa12339c5a2e0e4
KarolinaGolabek <karolina_golabek@poczta.onet.pl>
2019-12-08 03:45:55 +0100

    add login view

--- rawa/app.py
+++ rawa/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from os import environ
-
+from rawa.commands.exceptions import CommandError
 from rawa.models import db
+from rawa.commands.user import register, login as command_login
 
 DATABASE_URI = environ.get('DATABASE_URI', 'sqlite:///test.db')
 
@@ -10,18 +11,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 
 
 @app.route('/')
-def home(name=None):
-    return render_template('home.html', name=name)
+def home():
+    return render_template('home.html')
 
 
 @app.route('/login/')
-def login(name=None):
-    return render_template('login.html', name=name)
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = command_login(email, password)
+        if user is None:
+            return render_template('login.html', failed=True)
+        else:
+            session['user_id'] = user.id
+            return render_template('dashboard.html')
 
 
 @app.route('/register/')
-def register(name=None):
-    return render_template('register.html', name=name)
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            register(email, password)
+        except CommandError as exp:
+            return render_template('register.html', errs=exp.args[0])
+        else:
+            return render_template('register_done.html')
 
 
 if __name__ == '__main__':
