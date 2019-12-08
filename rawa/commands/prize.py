0fa019ffd7f8544899fc902c177a95aedf3f9b5e
n.kowalewska <n.kowalewska@clearcode.cc>
2019-12-08 11:40:04 +0100

    add tempate for unathorized

--- rawa/commands/prize.py
+++ rawa/commands/prize.py
@@ -7,23 +7,30 @@ def find_prize(prize_id: int) -> Optional[Prize]:
     return Prize.query.filter_by(id=prize_id).first()
 
 
+def get_all_prizes() -> List[Prize]:
+    return list(
+        Prize.query
+            .order_by(Prize.score)
+    )
+
+
 def get_available_prizes(user: User, user_score: int) -> List[Prize]:
     return list(
         Prize.query
-        .outerjoin(Prize.bought_prizes)
-        .filter(
+            .outerjoin(Prize.bought_prizes)
+            .filter(
             BoughtPrize.user != user,
             Prize.score <= user_score,
         )
-        .order_by(Prize.score)
+            .order_by(Prize.score)
     )
 
 
 def get_bought_prizes(user: User) -> List[Prize]:
     return list(
         Prize.query
-        .join(Prize.bought_prizes)
-        .filter(BoughtPrize.user == user)
+            .join(Prize.bought_prizes)
+            .filter(BoughtPrize.user == user)
     )
 
 
